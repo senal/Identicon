3218909caa01b50135c5b419993bb78d06dcd69f
Ranga Fernando <ranga.fernando@gmail.com>
2019-01-06 22:56:25 +1100

    More pattern matching

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -2,7 +2,13 @@ defmodule Identicon do
  def main(input) do
    input
    |> hash_input
+   |> pick_color
  end
+
+def pick_color(image) do
+  %Identicon.Image{hex: [r, g, b | _rest] } = image
+  [r, g, b]
+end
 
  def hash_input(input) do
     hex = :crypto.hash(:md5, input)
