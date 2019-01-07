bf72abf50cfca6a3bd60b79591139025c64319f3
Ranga Fernando <ranga.fernando@gmail.com>
2019-01-07 22:16:10 +1100

    More on pattern matching while build the Identicon

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,18 +1,22 @@
 defmodule Identicon do
- def main(input) do
-   input
-   |> hash_input
-   |> pick_color
- end
+  def main(input) do
+    input
+      |> hash_input
+      |> pick_color
+      |> build_grid
+  end
 
-def pick_color(image) do
-  %Identicon.Image{hex: [r, g, b | _rest] } = image
-  [r, g, b]
-end
+  def pick_color(%Identicon.Image{hex: [r, g, b | _rest] } = image) do
+    %Identicon.Image{image | color: {r, g, b}}
+  end
 
- def hash_input(input) do
-    hex = :crypto.hash(:md5, input)
-    |> :binary.bin_to_list
-    %Identicon.Image{hex: hex}
- end
+  def build_grid(image) do
+    
+  end
+
+  def hash_input(input) do
+      hex = :crypto.hash(:md5, input)
+      |> :binary.bin_to_list
+      %Identicon.Image{hex: hex}
+  end
 end
