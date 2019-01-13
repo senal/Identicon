751abe4a3205e47b5fc909fd42859ab790e7317f
Ranga Fernando <ranga.fernando@gmail.com>
2019-01-13 22:15:34 +1100

    Introduce real grid colouring

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -4,6 +4,7 @@ defmodule Identicon do
       |> hash_input
       |> pick_color
       |> build_grid
+      |> filter_odd_squares
   end
 
   def pick_color(%Identicon.Image{hex: [r, g, b | _rest] } = image) do
@@ -11,11 +12,21 @@ defmodule Identicon do
   end
 
   def build_grid(%Identicon.Image{hex: hex} = image) do
-    hex
-    |> Enum.chunk(3)
-    |> Enum.map(&mirror_row/1)
-    |> List.flatten
-    |> Enum.with_index
+    grid = 
+      hex
+      |> Enum.chunk(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+      
+      %Identicon.Image{image | grid: grid}
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid = Enum.filter grid, fn({code, _index}) ->
+        rem(code, 2) == 0
+    end
+    %Identicon.Image{image | grid: grid}
   end
 
   def mirror_row(row) do
